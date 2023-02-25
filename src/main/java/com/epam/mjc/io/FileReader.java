2dc98d58147113a8da1a5477968d3654845da152
Davronbek Sharipov <davronking001@gmail.com>
2023-02-25 17:46:38 +0500

    Complete io task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,42 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder res = new StringBuilder();
+        Profile profile = new Profile();
+        try (FileInputStream fs = new FileInputStream(file)) {
+            int c;
+            while ((c = fs.read()) != -1) {
+                res.append((char) c);
+            }
+        } catch (Exception e) {
+            throw new CustomException(e.getMessage());
+        }
+        String[] arr = res.toString().split("\\r?\\n|\\r");
+        for (String s : arr) {
+            String[] str = s.split(": ");
+            switch (str[0]) {
+                case "Name":
+                    profile.setName(str[1]);
+                    break;
+                case "Age":
+                    profile.setAge(Integer.parseInt(str[1]));
+                    break;
+                case "Email":
+                    profile.setEmail(str[1]);
+                    break;
+                case "Phone":
+                    profile.setPhone(Long.parseLong(str[1]));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return profile;
     }
 }
